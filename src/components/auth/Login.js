e3becd5964741ba4d377e68f0254c485b89396f8
William Briscoe <will.m.briscoe@gmail.com>
2023-06-14 15:27:47 -0500

    navBar started, logout implemented

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 import "./Login.css"
 
 export const Login = () => {
-    const [email, set] = useState("hpassfield7@netvibes.com")
+    const [email, set] = useState("my.man@my.guy")
     const navigate = useNavigate()
 
     const handleLogin = (e) => {
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,24 @@
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+
+    return (
+        <ul className="navbar">
+            {/* <li className="navbar__item active">
+                <Link className="navbar__link" to="/tickets">Tickets</Link>
+            </li> */}
+            {
+                localStorage.getItem("media_user")
+                    ? <li className="navbar__item navbar__logout">
+                        <Link className="navbar__link" to="" onClick={() => {
+                            localStorage.removeItem("media_user")
+                            navigate("/", {replace: true})
+                        }}>Logout</Link>
+                    </li>
+                    : ""
+            }
+        </ul>
+    )
+}
