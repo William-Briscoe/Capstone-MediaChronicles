29e2b0e1fb5902a1143561ef0283e55c6fc2db38
William Briscoe <will.m.briscoe@gmail.com>
2023-06-14 14:57:58 -0500

    inital commit

---
+++ src/components/Chronicles.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import { Authorized } from "./views/Authorized"
+//import { AppViews } from "./views/AppViews"
+//import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+//import "./Repairs.css"
+
+
+//react starting point
+export const Chronicles = () =>{
+    return <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register />}/>
+
+        <Route path="*" element={
+            <Authorized>
+                <>
+                    {/*<NavBar/>
+                    <AppViews />*/}
+                </>
+            </Authorized>
+        } />
+    </Routes>
+}
---
+++ src/components/auth/Register.js
@@ -0,0 +1,89 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+
+export const Register = (props) => {
+    const [customer, setCustomer] = useState({
+        email: "",
+        name: "",
+        password:"",
+        isAdmin: false
+    })
+    let navigate = useNavigate()
+
+    const registerNewUser = () => {
+        return fetch("http://localhost:8088/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(customer)
+        })
+            .then(res => res.json())
+            .then(createdUser => {
+                if (createdUser.hasOwnProperty("id")) {
+                    localStorage.setItem("media_user", JSON.stringify({
+                        id: createdUser.id,
+                        admin: createdUser.isAdmin
+                    }))
+
+                    navigate("/")
+                }
+            })
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+        return fetch(`http://localhost:8088/users?email=${customer.email}`)
+            .then(res => res.json())
+            .then(response => {
+                if (response.length > 0) {
+                    // Duplicate email. No good.
+                    window.alert("Account with that email address already exists")
+                }
+                else {
+                    // Good email, create user.
+                    registerNewUser()
+                }
+            })
+    }
+
+    const updateCustomer = (evt) => {
+        const copy = { ...customer }
+        copy[evt.target.id] = evt.target.value
+        setCustomer(copy)
+    }
+
+    return (
+        <main style={{ textAlign: "center" }}>
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Please Register for Honey Rae Repairs</h1>
+                <fieldset>
+                    <label htmlFor="name"> Full Name </label>
+                    <input onChange={updateCustomer}
+                        type="text" id="name" className="form-control"
+                        placeholder="Enter your name" required autoFocus />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="email"> Email address </label>
+                    <input onChange={updateCustomer}
+                        type="email" id="email" className="form-control"
+                        placeholder="Email address" required />
+                </fieldset>
+                <fieldset>
+                    <input onChange={(evt) => {
+                        const copy = { ...customer }
+                        copy.isAdmin = evt.target.checked
+                        setCustomer(copy)
+                    }}
+                        type="checkbox" id="isAdmin" />
+                    <label htmlFor="email"> I am an employee </label>
+                </fieldset>
+                <fieldset>
+                    <button type="submit"> Register </button>
+                </fieldset>
+            </form>
+        </main>
+    )
+}
+
